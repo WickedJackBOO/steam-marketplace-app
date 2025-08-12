012c7ff6ad26556af153c002ea0f4e3f0b5680e3
WickedJackBOO <77158704+WickedJackBOO@users.noreply.github.com>
2025-08-12 02:25:29 -0400

    start of using the info saved in file

--- myGames.py
+++ myGames.py
@@ -2,31 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 import xmltodict
 import json
+from datetime import datetime
+import os
+import time
 
 accountUrl = "https://steamcommunity.com/id/WickedJackBOO/games?xml=1"
 cardsUrl = "https://raw.githubusercontent.com/nolddor/steam-badges-db/main/data/badges.json"
 
-response = requests.get(accountUrl)
-soup = BeautifulSoup(response.content, "xml")
-xmlStr = str(soup)
+startRunTime = datetime.now()
+print(f"Start time {startRunTime}")
 
-jsonData = xmltodict.parse(xmlStr)
-# jsonStr = json.dumps(jsonData, indent=1)
+def getInfo(startRunTime, accountUrl, cardsUrl):
+    response = requests.get(accountUrl)
+    soup = BeautifulSoup(response.content, "xml")
+    xmlStr = str(soup)
 
-# with open('myGames.json', 'w') as f:
-    # json.dump(jsonData, f)
+    modTime = datetime.fromtimestamp(os.path.getmtime("myGames.json"))
+    print (modTime)
+    jsonData = xmltodict.parse(xmlStr)
+    jsonStr = json.dumps(jsonData, indent=1)
+    with open('myGames.json', 'w') as f:
+        json.dump(jsonData, f)
+
+    response = requests.get(cardsUrl)
+    GamesWithCardsData = response.json()
+    with open('cards.json', 'w') as f:
+        json.dump(GamesWithCardsData, f)
+    return 
 
 
+print (startRunTime - modTime)
 
 
-response = requests.get(cardsUrl)
-GamesWithCardsData = response.json()
-# with open('cards.json', 'w') as f:
-    # json.dump(GamesWithCardsData, f)
+# for game in jsonData["gamesList"]["games"]["game"]:
+#     if game["appID"] in GamesWithCardsData:
+#         print (game["name"])
 
 
-
-
-for game in jsonData["gamesList"]["games"]["game"]:
-    if game["appID"] in GamesWithCardsData:
-        print (game["name"])
